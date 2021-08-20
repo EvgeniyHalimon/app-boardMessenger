b98dd16b21b4f82d12619e230a1e954459d0bc2e
Hori <halimonevgeniy@gmail.com>
2021-08-20 17:09:57 +0300

    like fix

--- js/app.js
+++ js/app.js
@@ -11,9 +11,9 @@ board.addEventListener("click", () => {
 
 const account = Storage.getData() 
 
-getPost()
+printPost()
 
-postBtn.addEventListener("click", () => {
+postBtn.addEventListener("click", async () => {
     const body = {
         post: textarea.value,
         userId: account,
@@ -21,16 +21,14 @@ postBtn.addEventListener("click", () => {
         date: new Date().toLocaleDateString(),
         hours: new Date().toLocaleTimeString().slice(0,-3)
     }
-    Fetch.post("posts", body)
+    await Fetch.post("posts", body)
+    printPost()
 })
 
-async function getPost() {
-    let res= await Fetch.get(`posts?userId=${account}&_expand=user&_sort=likeQuantity,date,hours&_order=desc,desc,desc`)
-    printPost(res)
-}
-
-function printPost(arr) {
-    arr.forEach(async(item) =>{
+async function printPost() {
+    const posts = await Fetch.get(`posts?userId=${account}&_expand=user&_sort=likeQuantity,date,hours&_order=desc,desc,desc`)
+    postList.innerHTML = ""
+    posts.forEach(async(item) => {
         const elem = document.createElement("li")
         elem.classList.add("list-item")
         const spanPost = document.createElement("span")
@@ -44,14 +42,12 @@ function printPost(arr) {
         editBtn.classList.add("edit-btn")
         editBtn.innerHTML = "Edit"
         const input = document.createElement("input")
-        
         input.type = "checkbox"
         input.id = item.id
         const label = document.createElement("label")
         label.setAttribute('for', item.id)
         const likeQua = document.createElement("p")
 
-    
         elem.prepend(spanPost)
         commonSpan.appendChild(spanCheck)
         commonSpan.appendChild(spanBio)
@@ -72,27 +68,30 @@ function printPost(arr) {
         if(respLike.length === 1){
             input.checked = true
             input.disabled = true
-        } 
+        }
 
         label.addEventListener("click", async () => {
             const getLikeStatus = await Fetch.get(`likes?userId=${account}&postId=${item.id}`)
             const respLikeStatus = await getLikeStatus
-            if(respLikeStatus.length === 0 ){
+            console.log(respLikeStatus.length)
+            if(respLikeStatus.length === 0){
                 const bodyLikes = {
                     userId: account,
                     postId: item.id,
                     qua: 1
                 }
-                Fetch.post("likes", bodyLikes)
+                await Fetch.post("likes", bodyLikes)
                 const body = {
                     likeQuantity: item.likeQuantity + 1
                 }
-                Fetch.patch(`posts/${item.id}`, body)
+                await Fetch.patch(`posts/${item.id}`, body)
+                const currentLike = await Fetch.get(`posts/${item.id}`)
+                likeQua.innerHTML = currentLike.likeQuantity
             }
         })
 
         editBtn.addEventListener("click", () => {
-            editBtn.remove()
+            editBtn.style.display = "none"
             const input = document.createElement("input")
             input.classList.add("edit-input")
             input.value = spanPost.innerHTML
@@ -103,13 +102,16 @@ function printPost(arr) {
             input.type = "text"
             spanPost.prepend(input)
             spanPost.appendChild(saveBtn)
-            saveBtn.addEventListener("click", () => {
+            saveBtn.addEventListener("click", async () => {
                 input.remove()
                 saveBtn.remove()
                 const body = {
                     post: input.value
                 }
-            Fetch.patch(`posts/${item.id}`, body) 
+            await Fetch.patch(`posts/${item.id}`, body) 
+            const currentValue = await Fetch.get(`posts/${item.id}`)
+            spanPost.innerHTML = currentValue.post
+            editBtn.style.display = "block"
             })
         })
     })
